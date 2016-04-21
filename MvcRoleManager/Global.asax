3f13edae7cacdc494e2d5c273128b6a09c43cee5
Neo He <popman.he@gmail.com>
2016-04-20 22:18:55 -0700

    Change the namespace

--- MvcRoleManager/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcRoleManager.WebApiApplication" Language="C#" %>
--- MvcRoleManager/Startup.cs
+++ MvcRoleManager/Startup.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
 
 [assembly: OwinStartup(typeof(MvcRoleManager.Startup))]
 
@@ -12,6 +16,12 @@ namespace MvcRoleManager
     {
         public void Configuration(IAppBuilder app)
         {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
             ConfigureAuth(app);
         }
     }
